4fbcc524f5ee1c24cdeceb413306d781f0497fa7
Chang Niu <47059421+changniu54@users.noreply.github.com>
2022-10-21 19:17:29 +0800

    Delete gzsl_apy.sh

--- ufg_script/gzsl_apy.sh
+++
@@ -1,7 +0,0 @@
-python main.py \
---gpu_id 6 --manualSeed 9182 --cls_weight 0.01 --preprocessing \
---lr 0.00001 --cuda --image_embedding res101 --class_embedding att \
---nepoch 30 --syn_num 1800 --ngh 4096 --ndh 4096 --lambda1 10 \
---critic_iter 5 --nclass_all 32 --dataset APY --batch_size 64 --nz 64 --attSize 64 \
---resSize 2048 --outname apy --clb_time 20 --clb_weight 0.1 --clb_ratio 0.4 --neighbor_num 4
-
